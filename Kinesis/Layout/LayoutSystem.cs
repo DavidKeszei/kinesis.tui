b1a4d0ac967335dd1f9970229cb68e340e8b6bf5
Arcanium <76428399+DavidKeszei@users.noreply.github.com>
2026-03-31 22:07:12 +0200

    Revert "Feature/layout system"

--- Kinesis/Layout/LayoutSystem.cs
+++ Kinesis/Layout/LayoutSystem.cs
@@ -22,13 +22,10 @@ internal partial class LayoutSystem: IDynamicSystem {
     [return: MarshalAs(unmanagedType: UnmanagedType.Bool)]
     private static partial bool GetWindowScale(nint hnd, ref WindowsConsoleEventMsg buffer, uint length, out uint _);
 
-    [LibraryImport(libraryName: "kernel32.dll", EntryPoint = "GetStdHandle")]
-    private static partial nint GetStandardHandle(uint type);
-
     #endregion
 
     private readonly State<LayoutInfo> m_info = null!;
-    private nint m_stdHandle = nint.Zero;
+    private readonly nint m_stdHandle = nint.Zero;
 
     /// <summary>
     /// Behavior of the <see cref="LayoutSystem"/>.
@@ -39,8 +36,10 @@ internal partial class LayoutSystem: IDynamicSystem {
     /// Create a new <see cref="LayoutSystem"/> with <paramref name="scale"/>.
     /// </summary>
     /// <param name="scale">Start scale of the application. This is going be the pivot point of the observing.</param>
-    public LayoutSystem(State<LayoutInfo> state, Vec2 scale) {
+    public LayoutSystem(nint handle, State<LayoutInfo> state, Vec2 scale) {
         m_info = state;
+        m_stdHandle = handle;
+
         m_info.Value = new LayoutInfo(scale, IsChanged: false);
     }
 
@@ -53,8 +52,6 @@ internal partial class LayoutSystem: IDynamicSystem {
     }
 
     private void RunOnWindows() {
-        m_stdHandle = GetStandardHandle(uint.MaxValue - 10 + 1);
-
         while (true) {
             Thread.Sleep(millisecondsTimeout: POOLING_TIME);
 
--- Kinesis/UI/Island.cs
+++ Kinesis/UI/Island.cs
@@ -34,21 +34,23 @@ public abstract class Island {
     /// <param name="island">Current <see cref="Island"/> instance.</param>
     public static implicit operator Entity(Island island) => island.Build() ?? null!;
 
-        m_renderSet = new List<Entity>(capacity: 32);
-    }
+    public Island()
+        => m_renderSet = new List<Entity>(capacity: 32);
 
     /// <summary>
     /// Build the island as <see cref="Entity"/>.
     /// </summary>
-    /// <param name="context">Current target entity of the call.</param>
-    internal void CreateRenderSet(BuildContext context) {
-        if (context.Current is Island island) {
+    /// <returns>Return <see cref="Entity"/> instance from the current <see cref="Island"/>.</returns>
+    protected abstract Entity? Build();
 
-            Entity? created = island.Build(context);
-            if (created == null) return;
-
-            context.Current.GetComponent<Hierarchy>(index: Hierarchy.ChildrenStart)!.Attached = created;
-            created.GetComponent<Hierarchy>(index: Hierarchy.Parent)!.Attached = context.Current;
+    /// <summary>
+    /// Move through the tree and create list from it.
+    /// </summary>
+    /// <param name="entity">Current target entity of the call.</param>
+    internal void CreateRenderSet(Entity? entity = null!, int depth = 0) {
+        if (m_isTopOfTheTree) {
+            entity ??= Build();
+            m_isTopOfTheTree = false;
         }
 
         if(entity == null) return;
